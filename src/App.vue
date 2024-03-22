3bae82f1cf79a139d670a179bb33bcd71667877a
yuyijun <yuyijun@enn.cn>
2024-03-22 22:09:16 +0800

    表格初始

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Upload/>
+  <Table/>
 </template>
 <script lang="ts">
 import Upload from "./components/Upload.vue";
+import Table from './components/Table.vue'
 export default {
   name: "App",
-  components: {Upload},
+  components: {Upload,Table},
 };
 </script>
 
---
+++ src/components/Demo.vue
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/element-plus/dist/index.css"/>
+<!-- Import component library -->
+<script src="//cdn.jsdelivr.net/npm/element-plus"></script>
+<script setup name="Demo"></script>
